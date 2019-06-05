8a5688bcb7e88561bc180cb3e8fc1d11086df68c
Gerard Boyle <ger_boyle@yahoo.ie>
2019-06-05 12:45:34 +0000

    Building User resource

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+User.create(username: "Ger", password: "password")
+User.create(username: "James", password: "password")
+User.create(username: "Peter", password: "password")
+User.create(username: "Mary", password: "password")
+User.create(username: "Roger", password: "password")
